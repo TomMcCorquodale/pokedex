482630e53e42ede7d863045daab7b0ff8d7bfb83
Tommm10 <tommccorquodale@gmail.com>
2017-06-10 22:29:47 -0500

    Connected IBOutlets and added alamofire pod

--- pokedex/PokemonDetailViewController.swift
+++ pokedex/PokemonDetailViewController.swift
@@ -11,10 +11,29 @@ import UIKit
 class PokemonDetailViewController: UIViewController {
     
     var pokemon: Pokemon!
-
+    
+    @IBOutlet var mainImage: UIImageView!
+    @IBOutlet var descripLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var defenseLabel: UILabel!
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var pokedexIdLabel: UILabel!
+    @IBOutlet var attackLabel: UILabel!
+    @IBOutlet var currentEvoImg: UIImageView!
+    @IBOutlet var nextEvoImg: UIImageView!
+    @IBOutlet var evoLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
